4225c0c4c3e7ab4c6719397d174a3949bb14ac0c
Padmapriya <padmapriyajyoti99@gmail.com>
2023-01-19 13:54:33 +0530

    Manasi

---
+++ src/com/velocity/Employee.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+public class Employee {
+// How to compare two arrays are equal in size or not
+	public static void main(String[] args) {
+		int [] a = {1,2,3,4};
+		int [] b = {2,3,4,5};
+		if (a.length == b.length) {
+			System.out.println("its true");
+		}
+		else {
+			System.out.println("Its not true");
+		}
+
+	}
+
+}
