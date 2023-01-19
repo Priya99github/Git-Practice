9ad1db65146fdb5bf001e4e38d77e08c5696806c
Padmapriya <padmapriyajyoti99@gmail.com>
2023-01-19 14:29:37 +0530

    Values Changed

--- src/com/velocity/Employee.java
+++ src/com/velocity/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
 // How to compare two arrays are equal in size or not
 	public static void main(String[] args) {
 		int [] a = {1,2,3,4};
-		int [] b = {2,3,4,5};
+		int [] b = {3,4,5,6};
 		if (a.length == b.length) {
 			System.out.println("its true");
 		}
